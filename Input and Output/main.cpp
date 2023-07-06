735b47cea45765a730319266c26ff303b660410e
Kartavya Bang <bangkartavya@gmail.com>
2023-07-06 21:53:45 +0530

    7/6/2023

---
+++ Input and Output/main.cpp
@@ -0,0 +1,19 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int a;
+    int b;
+    int c;
+    
+    cin >> a >> b >> c;
+    
+    cout << a + b + c;
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    return 0;
+}
