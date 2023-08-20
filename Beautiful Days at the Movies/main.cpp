aa279c52db31d994e08fa809d35b62ca37c1005c
Kartavya Bang <bangkartavya@gmail.com>
2023-08-20 09:20:29 +0530

    8/20/2023

---
+++ Beautiful Days at the Movies/main.cpp
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'beautifulDays' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER i
+ *  2. INTEGER j
+ *  3. INTEGER k
+ */
+
+int beautifulDays(int i, int j, int k) {
+
+    int bDay =0;
+    
+    for(int day = i;day<=j;day++) {
+        string dayAsStr = to_string(day);
+        string reverseDay;
+        for(int p = dayAsStr.size()-1;p>=0;p--) {
+            reverseDay+=dayAsStr[p];
+        }
+        int reverseDayInt = stoi(reverseDay);
+        
+        if(abs(day-reverseDayInt)%k==0) {
+            bDay++;
+        }
+    }
+    return bDay;
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string first_multiple_input_temp;
+    getline(cin, first_multiple_input_temp);
+
+    vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+    int i = stoi(first_multiple_input[0]);
+
+    int j = stoi(first_multiple_input[1]);
+
+    int k = stoi(first_multiple_input[2]);
+
+    int result = beautifulDays(i, j, k);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
