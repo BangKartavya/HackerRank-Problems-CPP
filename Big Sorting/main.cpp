5df40d2c782fadf2d3964dabbee2745ac11271ce
Kartavya Bang <bangkartavya@gmail.com>
2023-08-28 14:13:38 +0530

    8/28/2023

---
+++ Big Sorting/main.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'bigSorting' function below.
+ *
+ * The function is expected to return a STRING_ARRAY.
+ * The function accepts STRING_ARRAY unsorted as parameter.
+ */
+
+vector<string> bigSorting(vector<string> u) {
+    sort(u.begin(), u.end(), [](string l, string r){
+        if(l.size() == r.size()) return l < r;
+        return l.size() < r.size();
+    });
+    return u;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    vector<string> unsorted(n);
+
+    for (int i = 0; i < n; i++) {
+        string unsorted_item;
+        getline(cin, unsorted_item);
+
+        unsorted[i] = unsorted_item;
+    }
+
+    vector<string> result = bigSorting(unsorted);
+
+    for (size_t i = 0; i < result.size(); i++) {
+        fout << result[i];
+
+        if (i != result.size() - 1) {
+            fout << "\n";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
