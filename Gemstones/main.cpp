5bba3e398316de8698bb146fe66675cbaf9393c6
Kartavya Bang <bangkartavya@gmail.com>
2023-09-24 18:54:46 +0530

    09/24/2023

---
+++ Gemstones/main.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'gemstones' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING_ARRAY arr as parameter.
+ */
+
+int gemstones(vector<string> arr) {
+    int result=0;
+
+    string alpha = "abcdefghijklmnopqrstuvwxyz";
+    
+    for(int i =0;i<26;i++) {
+        int cp = 0;
+        
+        for(int j=0;j<arr.size();j++) {
+            if(arr[j].find(alpha[i])!=string::npos) cp++;
+            
+        }
+        if(cp==arr.size()) result++;    
+        
+    }
+    return result;
+
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    vector<string> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        string arr_item;
+        getline(cin, arr_item);
+
+        arr[i] = arr_item;
+    }
+
+    int result = gemstones(arr);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
