23aa2b329c050d6e5204cef2f58ca883db628879
Kartavya Bang <bangkartavya@gmail.com>
2023-09-24 22:47:51 +0530

    09/24/2023

---
+++ Anagram/main.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'anagram' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING s as parameter.
+ */
+
+int anagram(string s) {
+    if(s.size() % 2 == 1) return -1;
+    map<char, int> mp;
+    int ans = 0;
+    for(int i = 0; i < s.size() / 2; i++) mp[s[i]]++;
+    for(int i = s.size() / 2; i < s.size(); i++){
+        if(mp[s[i]] != 0) mp[s[i]]--;
+        else ans++;
+    }
+    return ans;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string q_temp;
+    getline(cin, q_temp);
+
+    int q = stoi(ltrim(rtrim(q_temp)));
+
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        string s;
+        getline(cin, s);
+
+        int result = anagram(s);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
