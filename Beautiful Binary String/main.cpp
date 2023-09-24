eec44a589ddab1d82e9208cadb497e4a582208b4
Kartavya Bang <bangkartavya@gmail.com>
2023-09-24 19:02:57 +0530

    09/24/2023

---
+++ Beautiful Binary String/main.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'beautifulBinaryString' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING b as parameter.
+ */
+
+int beautifulBinaryString(string b) {
+    
+    int count = 0 ;
+
+    if(b=="010") return 1;
+    
+    for(int i =0;i<b.size();) {
+        string p = b.substr(i,3);
+        
+        if(p=="010") {
+            count++;
+            i+=3;
+        }
+        else i++;
+    }
+    return count;
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string b;
+    getline(cin, b);
+
+    int result = beautifulBinaryString(b);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
